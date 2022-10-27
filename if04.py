6ca08195e17161e96cde506f67a2be08fcd1fa4b
parkdin <abdukokhorovizatillo00@gmail.com>
2022-10-27 20:07:57 +0900

    done

--- if04.py
+++ if04.py
@@ -7,4 +7,12 @@ def main(a,b):
     Returns:
         int: return answer.
     """
-    return
+    if a == b:
+        return 0
+    if a != b:
+        if a > b:
+            return a
+        else:
+            return b
+    
+print(main(5,5))
